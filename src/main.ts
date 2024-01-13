5f4f3f3c542e82053ffb2fcbeb53e218da9f79fe
danju4rizzl <danjumashiwaju@gmail.com>
2024-01-13 20:17:18 +0200

    grabbing header links from 3rd-party site

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
-// For more information, see https://crawlee.dev/
-import { PlaywrightCrawler } from 'crawlee';
+import { CheerioCrawler } from 'crawlee'
 
-// PlaywrightCrawler crawls the web using a headless
-// browser controlled by the Playwright library.
-const crawler = new PlaywrightCrawler({
-    // Use the requestHandler to process each of the crawled pages.
-    async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-        const title = await page.title();
-        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+const crawler = new CheerioCrawler({
+  maxRequestsPerCrawl: 5,
 
-        // Save results as JSON to ./storage/datasets/default
-        await pushData({ title, url: request.loadedUrl });
+  // handles the processing request
+  async requestHandler({ $, request, enqueueLinks }) {
+    const title = $('title').text()
+    console.log(`The title of "${request.url}" is: ${title}.`)
+    // The default behavior of enqueueLinks is to stay on the same hostname,
+    // so it does not require any parameters.
+    // This will ensure the subdomain stays the same.
+    await enqueueLinks()
+  }
+})
 
-        // Extract links from the current page
-        // and add them to the crawling queue.
-        await enqueueLinks();
-    },
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
-    // Uncomment this option to see the browser window.
-    // headless: false,
-});
-
-// Add first URL to the queue and start the crawl.
-await crawler.run(['https://crawlee.dev']);
+await crawler.run(['https://punchng.com/'])
