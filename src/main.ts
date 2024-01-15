34c4fc25a65e001a62b11b6f9ee0902bcc5d7f96
danju4rizzl <danjumashiwaju@gmail.com>
2024-01-15 02:19:15 +0200

    added configuration via crawlee.json

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import { router } from './routes.js'
 // Define the URL to start the crawl from
 const START_URL = 'https://warehouse-theme-metal.myshopify.com/collections/'
 
-// This is better set with CRAWLEE_LOG_LEVEL env var
-// or a configuration option. This is just for show 😈
-log.setLevel(log.LEVELS.DEBUG)
-
-log.debug('Setting up crawler.')
 const crawler = new PlaywrightCrawler({
   // Instead of the long requestHandler with
   // if clauses we provide a router instance.
